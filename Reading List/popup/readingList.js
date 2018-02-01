8ccb44b6f0558a921f65c346ac17074dce3f096b
ruochen.xie <ruochen.xie@bitmain.com>
2018-02-01 15:20:19 +0800

    reading list first upload

---
+++ Reading List/popup/readingList.js
@@ -0,0 +1,9 @@
+const outlinePage = ` * {
+                        border: 1px solid red;
+                    }`;
+
+var browser = browser || chrome
+
+document.getElementById('save').addEventListener("click", function () {
+    console.log(window.location.href)
+})
